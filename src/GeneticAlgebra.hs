6fa5ea14a3356cc917fd3e8100890357ffd623b6
Noah Thomas Jones <noahtjones@gmail.com>
2025-01-05 21:10:55 -0500

    nm

--- src/GeneticAlgebra.hs
+++ src/GeneticAlgebra.hs
@@ -1,3 +1,15 @@
 module GeneticAlgebra
     ( 
     ) where
+
+-- we have population, organism, chromosome levels which contain
+-- dominant or recessive alleles
+
+data Allele = D | R
+
+data Chromosome = Chromosome Allele
+
+data Organism = Organism Chromosome Chromosome
+
+data Population = Population [Organism]
+
