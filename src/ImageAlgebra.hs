c5c2169e13a93c9f1bc504170714329bb84e2969
Noah Thomas Jones <noahtjones@gmail.com>
2025-01-06 21:24:40 -0500

    working on gadts

--- src/ImageAlgebra.hs
+++ src/ImageAlgebra.hs
@@ -1,1 +1,16 @@
-module ImageAlgebra where
+{-# LANGUAGE GADTs ,
+ImportQualifiedPost #-}
+
+module ImageAlgebra (
+                    ) where
+
+import Data.ByteString qualified as BS
+
+data Image = Image BS.ByteString
+
+class Convolvable
+
+data FourierResult = FourierResult
+  { frequency :: Image
+  , phase :: Image
+  }
