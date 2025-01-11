8ee4c668ff52133490ca540f39d0bfda508170c7
Noah Jones <noahtjones@gmail.com>
2025-01-10 21:57:31 -0500

    Update ImageAlgebra.hs

--- src/ImageAlgebra.hs
+++ src/ImageAlgebra.hs
@@ -20,3 +20,14 @@ class Fourierable
 -- some quality regarding smoothness
 
 -- need subtractability and direct subtract direct sum
+
+class DirectAdd where
+  add = decomposed parts with check for same dimensions
+
+
+polymorphism for those with different dimensions
+
+given a single number to be added just add it to the whole thing or loop through smaller number for all points
+
+
+convolution should convolve with same local connectedness
